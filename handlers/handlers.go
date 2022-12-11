c89e7459d8ea581a686d6d3d3b4aeb794937c9eb
Jamie Cox <jamie.cox@genomicsengland.co.uk>
2022-12-11 16:52:51 +0000

    Module 2 POST

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -29,12 +29,21 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 		}
 		res.Write(filteredWinners)
 	}
-	
+
 }
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	if isTokenValid := data.IsAccessTokenValid(accessToken); !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		if err := data.AddNewWinner(req.Body); err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
